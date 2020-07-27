1ac38efde04d7002036b3c676566e693967f3c2e
ved.sachdeva <ved.sachdeva@curefit.com>
2020-07-27 20:34:36 +0530

    Updates

---
+++ precache-manifest.01e7de63f646bb87bb869a6832b8c9a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53bbf407e0c54f54da15bbfc087e95f",
+    "url": "/aurodhan/index.html"
+  },
+  {
+    "revision": "a9ea655bea08141ef19f",
+    "url": "/aurodhan/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "fb8d66be23a0d94cea24",
+    "url": "/aurodhan/static/css/main.fe0f51a5.chunk.css"
+  },
+  {
+    "revision": "a9ea655bea08141ef19f",
+    "url": "/aurodhan/static/js/2.354a0689.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/aurodhan/static/js/2.354a0689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8d66be23a0d94cea24",
+    "url": "/aurodhan/static/js/main.477203bf.chunk.js"
+  },
+  {
+    "revision": "8a3c3fc42b048be36b44",
+    "url": "/aurodhan/static/js/runtime-main.d49990cb.js"
+  }
+]);
